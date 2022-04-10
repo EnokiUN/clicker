2a671a6a0ad5563f9def9e2ccee3b511d8277999
EnokiUN <enokiun@gmail.com>
2022-04-10 23:58:54 +0300

    Added more stuff

--- src/main.gd
+++ src/main.gd
@@ -1,7 +1,10 @@
 extends Control
 
-export var cash: int = 0
+var cash: int = 0
+var clicked: int = 0
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+export var mincash: int = 0
+export var maxcash: int = 100
 var counting: bool = false
 var elapsed: float = 0
 
@@ -14,12 +17,13 @@ func _ready():
 func _process(delta):
 	if counting:
 		elapsed += delta
-		timer_label.text = "Time: %.2f" % elapsed
+		timer_label.text = "Time: %.2fs\nClicks: %d\nCPS: %.2fs\nAVG coins/click: %.2fs SusCoins" % [elapsed, clicked, clicked / elapsed, float(cash) / clicked]
 
 func _on_MoneyButton_pressed():
 	if !counting:
 		counting = true
-	cash += rng.randi_range(1, 100)
+	cash += rng.randi_range(mincash, maxcash)
+	clicked += 1
 	if cash >= 10_000:
 		counting = false
 	coin_label.text = "%d SusCoins" % cash
